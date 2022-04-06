6cd4cc5a6a82b6f40568b326d1663e601a22fe5a
farulwananda <farulahmad2002@gmail.com>
2022-04-06 23:00:50 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dicoding_wisata/detail_screen.dart';
+import 'package:flutter_dicoding_wisata/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: MainScreen(),
+    );
+  }
+}
